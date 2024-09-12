38dd99947d2366bfb33c9a3a39a8f7e4aa490546
pankajyadav8523 <py9621008569@gmail.com>
2024-09-12 15:09:37 +0530

    handling the empty input

--- src/components/chatBox/ChatBox.jsx
+++ src/components/chatBox/ChatBox.jsx
@@ -1,10 +1,29 @@
-import React from "react";
+import React, { useState } from "react";
 import "./ChatBox.css";
 
-const ChatBox = () => {
+const ChatBox = ({ setMessage }) => {
+  const [inputValue, setInputValue] = useState("");
+
+  const handleInputChange = (e) => {
+    setInputValue(e.target.value);
+  };
+
+  const handleKeyDown = (e) => {
+    if (e.key === "Enter" && inputValue.trim() !== "") {
+      setMessage(inputValue);
+      setInputValue("");
+    }
+  };
+
   return (
     <div className="container chatbox">
-      <input placeholder="Type your message" />
+      <input
+        type="text"
+        placeholder="Type your message"
+        onChange={handleInputChange}
+        onKeyDown={handleKeyDown}
+        value={inputValue}
+      />
     </div>
   );
 };
