5da8722e91236e5eaebac1b73eb197a67065ed7d
pankajyadav8523 <py9621008569@gmail.com>
2024-09-12 15:07:28 +0530

    storing previous message to display and checking the bot and user

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,23 @@
-import React from "react";
+import React, { useState } from "react";
 import "./App.css";
 import ChatScreen from "./components/chatScreen/ChatScreen";
 import ChatBox from "./components/chatBox/ChatBox";
 
 function App() {
+  const [messages, setMessages] = useState([]);
+
+  const handleSetMessage = (newMessage) => {
+    setMessages((prevMessages) => [
+      ...prevMessages,
+      { text: newMessage, sender: "user" },
+      { text: newMessage, sender: "bot" },
+    ]);
+  };
+
   return (
     <div className="App">
-      <ChatScreen />
-      <ChatBox />
+      <ChatScreen messages={messages} />
+      <ChatBox setMessage={handleSetMessage} />
     </div>
   );
 }
