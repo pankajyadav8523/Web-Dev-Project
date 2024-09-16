92e5952db8ff2067f62f7bc810e8f00a5d292753
pankajyadav8523 <py9621008569@gmail.com>
2024-09-16 16:18:02 +0530

    making transformation function

--- src/components/Output/BotOutput.jsx
+++ src/components/Output/BotOutput.jsx
@@ -1,11 +1,26 @@
 import React from "react";
 
-const BotOutput = ({ message }) => {
-  const convertLowercase = (text) => {
-    return text.toLowerCase();
-  };
+const transformations = {
+  countWords: (text) => text.split(" ").length,
+  reverseText: (text) => text.split("").reverse().join(""),
+  countCharacters: (text) => text.length,
+  removeVowels: (text) => text.replace(/[aeiouAEIOU]/g, ""),
+  replaceSpaces: (text) => text.replace(/\s+/g, "_"),
+};
 
-  return <div className="message-text">{convertLowercase(message)}</div>;
+const BotOutput = ({ message, transformation }) => {
+  const transformedMessage = transformations[transformation]
+    ? transformations[transformation](message)
+    : message;
+  const transformationLabel = transformation
+    .replace(/([A-Z])/g, " $1")
+    .toUpperCase();
+
+  return (
+    <div className="message-text">
+      {`${transformationLabel}: ${transformedMessage}`}
+    </div>
+  );
 };
 
 export default BotOutput;
