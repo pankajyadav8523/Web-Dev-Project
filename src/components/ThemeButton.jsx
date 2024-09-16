7e1a4f315da7307ac0b9f9a277d469566ec6cd63
pankajyadav8523 <py9621008569@gmail.com>
2024-09-16 22:20:17 +0530

    adding dark theme

---
+++ src/components/ThemeButton.jsx
@@ -0,0 +1,30 @@
+import React, { useState, useEffect } from "react";
+import { FaMoon, FaSun } from "react-icons/fa";
+
+export default function ThemeButton() {
+  const [currentTheme, setCurrentTheme] = useState("light");
+
+  useEffect(() => {
+    const rootElement = document.getElementById("root");
+
+    if (currentTheme === "light") {
+      rootElement.style.setProperty("--background-color", "cyan"); // Light background color
+      rootElement.style.setProperty("--navbar-color", "#f5f5f5"); // Light text color
+      rootElement.style.setProperty("--text-color", "#0040ff");
+    } else {
+      rootElement.style.setProperty("--background-color", "#2c3e50"); // Dark background color
+      rootElement.style.setProperty("--navbar-color", "#f5f5f5"); // Dark text color
+      rootElement.style.setProperty("--text-color", "blue");
+    }
+  }, [currentTheme]);
+
+  const handleClick = () => {
+    setCurrentTheme(currentTheme === "light" ? "dark" : "light");
+  };
+
+  return (
+    <button onClick={handleClick} className="theme-button">
+      {currentTheme === "light" ? <FaMoon size={20} /> : <FaSun size={20} />}
+    </button>
+  );
+}
