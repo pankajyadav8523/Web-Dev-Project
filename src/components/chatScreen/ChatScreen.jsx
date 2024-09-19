34fea62ba8d822bdc57ff2d07dd354a80ccab092
shivam Kumar <shivam2377kumar@gmail.com>
2024-09-19 11:10:22 +0530

    some comment and reviewed the bot  output component

--- src/components/chatScreen/ChatScreen.jsx
+++ src/components/chatScreen/ChatScreen.jsx
@@ -38,7 +38,7 @@ const ChatScreen = ({ messages }) => {
             )}
           </div>
         ))}
-        <div ref={messagesEndRef} />
+        <div ref = {messagesEndRef} />
       </div>
     </div>
   );
